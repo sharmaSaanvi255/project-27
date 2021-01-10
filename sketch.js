6adcc9c72ee56cc582ec88ad1bd8d6258678c8d9
sharmaSaanvi255 <72978396+sharmaSaanvi255@users.noreply.github.com>
2021-01-10 13:44:32 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,48 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	bob1=new Bob(100,320,70,70);
+	bob2 = new Bob(250,320,70,70);
+	bob3 = new Bob(400,320,70,70);
+	bob4 = new Bob(650,320,70,70);
+	bob5 = new Bob(800,320,70,70);
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+
+  bob1.display();
+  bob2.display();
+  bob3.display();
+  bob4.display();
+  bob5.display();
+
+
+  drawSprites();
+ 
+}
+
+
+
